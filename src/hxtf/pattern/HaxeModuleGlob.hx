47e8c29ecd2c9862764cd15885c88859c768f391
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-07-07 20:25:32 +0930

    Removed HaxeModuleGlobHelper.

--- src/hxtf/pattern/HaxeModuleGlob.hx
+++ src/hxtf/pattern/HaxeModuleGlob.hx
@@ -1,21 +1,15 @@
 package hxtf.pattern;
 
-using hxtf.pattern.HaxeModuleGlobHelper;
-
 /**
     This class represents a glob expression (converted into an EReg) designed to
     match package strings (eg `"my.pack"`, `"my.pack.Type"`, `"Type"`, etc).
 **/
 class HaxeModuleGlob extends Glob {
     public function new(raw:RawGlob) {
-        if (!HaxeModuleGlobHelper.isValid(raw)) {
+        if (!~/^(([*0-9?A-Z_a-z])|(([*0-9?A-Z_a-z][.*0-9?A-Z_a-z]*[*0-9?A-Z_a-z])|(\[!?(([.0-9A-Z_a-z])|([.0-9A-Z_a-z]-[.0-9A-Z_a-z]))+\]))+)$/.match(raw)) {
             throw HaxeModuleGlobException.InvalidGlob;
         }
         super(raw);
-    }
-
-    public inline override function hashCode():Int {
-        return HaxeModuleGlobHelper.hashCode(raw);
     }
 }
 
