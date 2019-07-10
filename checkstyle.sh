8345307696848d70aea29f8bb609268b2c44095b
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-07-10 10:21:42 +0930

    Simplified and made sh compliant.

--- checkstyle.sh
+++ checkstyle.sh
@@ -1,16 +1,2 @@
-#!/usr/bin/env bash
-
-SOURCE_DIRS=(
-    src
-    framework/src/hxtf
-);
-
-
-for ((i=0; i<${#SOURCE_DIRS[*]}; i++));
-do :
-    SOURCE_DIRS[$i]="-s ${SOURCE_DIRS[$i]}";
-done
-
-printf "\n";
-haxelib run checkstyle ${SOURCE_DIRS[*]};
+haxelib run checkstyle -s src -s framework/src/hxtf;
 printf "\n";
--- hxformat.sh
+++ hxformat.sh
@@ -1,16 +1,2 @@
-#!/usr/bin/env bash
-
-SOURCE_DIRS=(
-    src
-    framework
-);
-
-
-for ((i=0; i<${#SOURCE_DIRS[*]}; i++));
-do :
-    SOURCE_DIRS[$i]="-s ${SOURCE_DIRS[$i]}";
-done
-
 printf "\n";
-haxelib run formatter -v ${SOURCE_DIRS[*]};
-printf "\n";
+haxelib run formatter -v -s src -s framework;
