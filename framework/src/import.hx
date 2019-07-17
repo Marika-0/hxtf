b4d5bb150081e5a6a749b4cfd1d9575302c43d58
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-07-17 23:39:21 +0930

    Added commenting.

--- framework/src/import.hx
+++ framework/src/import.hx
@@ -1,4 +1,9 @@
+// Convenience for extending test cases and objects.
 import hxtf.TestCase;
 import hxtf.TestObject;
+
+// Something-or-other with macro's and derived classes. If these aren't imported
+// then test object can't use them, so we just import them here to not need to
+// import them in every test object file.
 import hxtf.TestObject.addCase;
 import hxtf.TestObject.addSuite;
