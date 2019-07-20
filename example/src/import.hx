de25c15db2b8686e74b5f9d62fe7c88cd07a03a9
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-07-20 17:29:47 +0930

    Working on haxelib

--- example/src/import.hx
+++
@@ -1,9 +0,0 @@
-// Convenience for extending test cases and distributors.
-import hxtf.TestCase;
-import hxtf.TestBroker;
-
-// Something-or-other with macro's and derived classes. If these aren't imported
-// then test object can't use them, so we just import them here to not need to
-// import them in every test object file.
-import hxtf.TestBroker.addTest;
-import hxtf.TestBroker.addBroker;
---
+++ project/import.hx
@@ -0,0 +1,8 @@
+// Convenience for extending test cases and distributors.
+import hxtf.TestCase;
+import hxtf.TestBroker;
+
+// Something with macros and derived classes means these need to be imported.
+// We're importing them here instead of adding a line to each Broker package.
+import hxtf.TestBroker.addTest;
+import hxtf.TestBroker.addBroker;
