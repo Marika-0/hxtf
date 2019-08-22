66221ec87d261569629c1cd0fd9e84479d939464
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-08-22 16:13:22 +0930

    Now prints failed process code.

--- project/src/hxtf/Compile.hx
+++ project/src/hxtf/Compile.hx
@@ -14,20 +14,24 @@ class Compile {
         stdout('[1mCompiling target: $target[0m\n');
 
         if (Flags.writeCompilationOutput) {
-            if (Sys.command("haxe _.hxml") != 0) {
-                stderr('\n[3mCompilation failed for target: $target[0m');
-                stdout("\n");
+            var code = Sys.command("haxe _.hxml");
+            if (code != 0) {
+                stderr('\n[3mCompilation failed for target: $target[0m\n');
+                stderr('[3mProcess exited with code: $code[0m\n');
                 Exit.elevate(TestRunCompilationFailure);
                 return false;
             }
         } else {
             var process = new Process("haxe _.hxml");
-            if (process.exitCode() != 0) {
-                stderr('[3mCompilation failed for target: $target[0m\n');
+            var code = process.exitCode();
+            if (code != 0) {
                 stderr("[41;1m" + stripAnsi(process.stderr.readAll().toString()) + "[0m");
+                stderr('[3mCompilation failed for target: $target[0m\n');
+                stderr('[3mProcess exited with code: $code[0m\n');
                 Exit.elevate(TestRunCompilationFailure);
                 return false;
             }
+            process.close();
         }
 
         return true;
