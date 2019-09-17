1f203014e10a69d462f9e9a87af37572f78db65b
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2019-09-17 12:56:54 +0930

    Improved comment.

--- resources/import.hx
+++ resources/import.hx
@@ -2,7 +2,6 @@
 import hxtf.TestCase;
 import hxtf.TestBroker;
 
-// Something with macros and derived classes means these need to be imported.
-// We're importing them here instead of adding a line to each Broker package.
+// Imported to resolve macro ambiguity.
 import hxtf.TestBroker.addTest;
 import hxtf.TestBroker.addBroker;
