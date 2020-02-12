e11aad25a439b42ad42e98b9705bad3e29291b3c
Marika-0 <48089643+Marika-0@users.noreply.github.com>
2020-02-12 14:21:07 +1030

    General improvements.

--- project/src/hxtf/Exit.hx
+++ project/src/hxtf/Exit.hx
@@ -1,35 +1,48 @@
 package hxtf;
 
+/**
+    Handles exiting HxTF with the appropriate elevated exit code.
+
+    The code that HxTF exits with can vary depending on the result of run tests.
+**/
 class Exit {
-    static var status:Status = Passing;
+    /**
+        The current code to exit with.
+    **/
+    static var status:Code = SUCCESS;
 
-    public static function elevate(code:Status) {
-        if (eval(status) < eval(code)) {
+    /**
+        Elevates the code used to exit the process.
+    **/
+    public static function elevate(code:Code):Void {
+        if (realise(status) < realise(code)) {
             status = code;
-        }
-    }
-
-    static function eval(x:Status) {
-        return switch (x) {
-            case Passing: 0;
-            case HxtfRuntimeFailure: 1;
-            case TestRunAssertionFailure: 2;
-            case TestRunRuntimeFailure: 3;
-            case TestRunCompilationFailure: 4;
-            default: -1;
         }
     }
 
     @:allow(hxtf.Hxtf.main)
     static function exit() {
-        Sys.exit(eval(status));
+        Sys.exit(realise(status));
+    }
+
+    static function realise(code:Code):Int {
+        return switch (code) {
+            case SUCCESS: 0;
+            case HXTF_RUNTIME_FAILURE | null: 1;
+            case TEST_ASSERTION_FAILURE: 2;
+            case TEST_RUNTIME_FAILURE: 3;
+            case TEST_COMPILATION_FAILURE: 4;
+        }
     }
 }
 
-enum Status {
-    Passing;
-    HxtfRuntimeFailure;
-    TestRunAssertionFailure;
-    TestRunRuntimeFailure;
-    TestRunCompilationFailure;
+/**
+    Appropriate exit codes.
+**/
+enum Code {
+    SUCCESS;
+    HXTF_RUNTIME_FAILURE;
+    TEST_ASSERTION_FAILURE;
+    TEST_RUNTIME_FAILURE;
+    TEST_COMPILATION_FAILURE;
 }
